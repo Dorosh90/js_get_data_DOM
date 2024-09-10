3ce6c8f597e385bf75bc28daba013df08667b4d5
dima <dima98doroshenko@gmail.com>
2024-09-10 21:09:20 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+let population = [
+    ...document.querySelectorAll(".population")
+];
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+population = population.map((country)=>{
+    const normalNumber = country.textContent.replaceAll(",", "");
+    return parseInt(normalNumber);
+});
+const sumAllPopulation = population.reduce((total, country)=>{
+    return total + country;
+}, 0);
+const averagePopulationValue = sumAllPopulation / population.length;
+totalPopulation.textContent = sumAllPopulation.toLocaleString("en-US");
+averagePopulation.textContent = averagePopulationValue.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
